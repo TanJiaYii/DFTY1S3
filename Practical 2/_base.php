3d35dcfd19cdb04523cb2ba41edf1babbbbe5131
TanJiaYii <tanjy-wm25@student.tarc.edu.my>
2026-02-04 09:59:53 +0800

    practical 2

---
+++ Practical 2/_base.php
@@ -0,0 +1,26 @@
+<?php
+
+// ============================================================================
+// PHP Setups
+// ============================================================================
+
+date_default_timezone_set('Asia/Kuala_Lumpur');
+
+// ============================================================================
+// General Page Functions
+// ============================================================================
+
+// Is GET request?
+// TODO
+
+// Is POST request?
+// TODO
+
+// Obtain GET parameter
+// TODO
+
+// Obtain POST parameter
+// TODO
+
+// Obtain REQUEST (GET and POST) parameter
+// TODO
